01f1c08011df4d3a4b7eeb6850574e8d8dae0c6f
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2022-11-22 22:43:09 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random()*10);
+
+const num3 = 32;
+const num4 = 7;
+const mod = 32 % 7;
+
+const max = Math.max(1,5,6,8,20);
+console.log(multiply)
